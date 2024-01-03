6805b57bac5fd960552cf7baa8f080a888bd1304
Xavier Messado <xavier@popshap.com>
2024-01-03 15:50:02 -0500

    Update App.tsx
    Formatting
    Fix useCallback Submission handler

--- client/App.tsx
+++ client/App.tsx
@@ -8,31 +8,36 @@ import { CollectionReference, addDoc } from 'firebase/firestore';
 
 // Define the type for AppProps
 interface AppProps {
-    colRef: CollectionReference;
+  colRef: CollectionReference;
 }
 
 const App: React.FC<AppProps> = ({ colRef }) => {
-    //state
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [score, setScore] = useState<any>('');
+  //state
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [score, setScore] = useState<any>('');
 
 
-    //submit function (sends user info to database) when user clicks button
-    const sendToDatabase = useCallback(() => {
-        
-        console.log("sending to database!")
-        addDoc(colRef, { firstName, lastName, score: Number(score)});
-        setFirstName('');
-        setLastName('');
-        setScore('');
-    }, []);
+  //submit function (sends user info to database) when user clicks button
+  const sendToDatabase = useCallback(async () => {
 
-    //resetting state once user makes input
-  function inputHandler(event: React.ChangeEvent<HTMLInputElement>){
+    console.log("sending to database!", {
+
+      firstName, lastName, score: Number(score)
+    })
+    await addDoc(colRef, { firstName, lastName, score: Number(score) });
+    setFirstName('');
+    setLastName('');
+    setScore('');
+  }, [firstName, lastName, score]);
+
+  //resetting state once user makes input
+  function inputHandler(event: React.ChangeEvent<HTMLInputElement>) {
     event.preventDefault();
 
-    switch(event.target.id){
+    console.log(event.target.id, event.target.value);
+
+    switch (event.target.id) {
       case "firstName":
         setFirstName(event.target.value);
         break;
@@ -45,26 +50,26 @@ const App: React.FC<AppProps> = ({ colRef }) => {
     }
   }
 
-    
-    return (
-        <BrowserRouter>
-        <Title/>
-         <Routes>
-   
-            <Route path="/" element={<AddScorePage 
-            firstName={firstName}
-            lastName={lastName}
-            score={score}
-            inputHandler={inputHandler}
-            colRef={colRef}/>} />
-            <Route path="scores" element={<ShowScoresPage colRef={colRef}/>} />
-      
-         </Routes>
-         <Button sendToDatabase={sendToDatabase}/>
-         
-        </BrowserRouter>
-      
-    )
+
+  return (
+    <BrowserRouter>
+      <Title />
+      <Routes>
+
+        <Route path="/" element={<AddScorePage
+          firstName={firstName}
+          lastName={lastName}
+          score={score}
+          inputHandler={inputHandler}
+          colRef={colRef} />} />
+        <Route path="scores" element={<ShowScoresPage colRef={colRef} />} />
+
+      </Routes>
+      <Button sendToDatabase={sendToDatabase} />
+
+    </BrowserRouter>
+
+  )
 }
 
 
